0b4b7a43084a8c5cd31215b4ec0dec9b048076a4
Tatiana Muniz <tati.muniz0@gmail.com>
2021-07-18 12:18:35 +0300

    Fix: auth route

--- pages/index.js
+++ pages/index.js
@@ -197,7 +197,7 @@ export async function getServerSideProps(context) {
   const cookies = nookies.get(context);
   const token = cookies.USER_TOKEN;
   const { isAuthenticated } = await fetch(
-    "http://localhost:3000/api/auth", {
+    "api/auth", {
     headers: {
       Authorization: token,
     },
